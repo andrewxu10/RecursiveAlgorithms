bd2a984bf38ea8006342da7668024c9afdc7d3a0
Andrew Xu <andrewxu_2016@depauw.edu>
2017-06-20 17:17:19 -0700

    started nqueens

---
+++ src/nQueens.java
@@ -0,0 +1,7 @@
+//This program takes an integer 'n' and creates an n*n Chess board. It then prints all of the combinations where n queens can be placed on the board
+public class nQueens {
+
+	public static void main (String[] args) {
+		System.out.println("hello");
+	}
+}
